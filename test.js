c6eb870f6a787ae46ddce9c916c785a667f61390
Naveen Ramakrishna <naveenrk18@gmail.com>
2018-01-15 16:22:08 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert('test');
+videojs.registerPlugin('listenForParent', function() {
+  var myPlayer = this;
+  // This method called when postMessage sends data into the iframe
+  function controlVideo(evt){
+    if(evt.data === "playVideo") {
+      myPlayer.play();
+    } else if (evt.data === 'pauseVideo') {
+      myPlayer.pause();
+    }
+  };
+  // Listen for the message, then call controlVideo() method when received
+  window.addEventListener("message",controlVideo);
+});
