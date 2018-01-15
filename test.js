b2fde51f553ea22d1f77d519b79a759c0ab134be
Naveen Ramakrishna <naveenrk18@gmail.com>
2018-01-15 16:37:00 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-document.getElementByClassName("vjs-big-play-button").addEventListener('click',function () {
+document.getElementsByClassName("vjs-big-play-button").addEventListener('click',function () {
           alert("BC js video called")
   
         })
-document.getElementByClassName("vjs-play-control").addEventListener('click', function () {
+document.getElementsByClassName("vjs-play-control").addEventListener('click', function () {
           alert("BC js Play and Pause video called")
         })
